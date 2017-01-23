df029ed6b3059083ddf86221c4ace095590d3dd2
Martin Hellwig <martinhellwig@gmx.de>
2017-01-23 19:08:17 +0100

    Fixed crash while coming from flight-mode

--- app/src/main/java/de/schooltec/datapass/ConnectionChangeReceiver.java
+++ app/src/main/java/de/schooltec/datapass/ConnectionChangeReceiver.java
@@ -32,47 +32,57 @@ public class ConnectionChangeReceiver extends BroadcastReceiver
         // was the last update event more than the specified time in the past?
         if (((new Date()).getTime() - lastChangeEventTimeStamp) > MIN_TIME_BETWEEN_TWO_REQUESTS)
         {
-            lastChangeEventTimeStamp = (new Date()).getTime();
-
-            // wait 3 seconds, so that phone has hopefully inet connection
-            try
-            {
-                Thread.sleep(3000);
-            }
-            catch (InterruptedException ignored)
-            {
-            }
-
-            // Get all appIds
-            SharedPreferences sharedPref = context.getSharedPreferences(PreferenceKeys.
-                    PREFERENCE_FILE_MISC, Context.MODE_PRIVATE);
-            Set<String> storedAppIds = sharedPref.getStringSet(PreferenceKeys.SAVED_APP_IDS, new HashSet<String>());
-
             // Get connection mode
             ConnectivityManager connectivityManager = (ConnectivityManager) context.
                     getSystemService(Context.CONNECTIVITY_SERVICE);
             NetworkInfo activeInfo = connectivityManager.getActiveNetworkInfo();
 
-            UpdateWidgetTask.Mode updateMode = UpdateWidgetTask.Mode.ULTRA_SILENT;
-            if (activeInfo != null && activeInfo.getType() == ConnectivityManager.TYPE_MOBILE)
+            if (activeInfo != null)
             {
-                /*
-                Only update in silent mode if switched to mobile data. Otherwise you are probably
-                in wifi mode, ergo there is no new information about the used data and therefore
-                grayish the progress bar.
-                 */
-                updateMode = UpdateWidgetTask.Mode.SILENT;
-            }
+                lastChangeEventTimeStamp = (new Date()).getTime();
 
-            // make new task for every widget
-            for (String storedAppId : storedAppIds)
-            {
-                int appWidgetId = Integer.valueOf(storedAppId.substring(0, storedAppId.
-                        indexOf(",")));
-                String carrier = storedAppId.substring(storedAppId.indexOf(",") + 1);
+                // wait 3 seconds, so that phone has hopefully inet connection
+                try
+                {
+                    Thread.sleep(3000);
+                }
+                catch (InterruptedException ignored)
+                {
+                }
 
-                new UpdateWidgetTask(appWidgetId, context, updateMode, carrier)
-                        .executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+                // Get all appIds
+                SharedPreferences sharedPref = context.getSharedPreferences(PreferenceKeys.
+                        PREFERENCE_FILE_MISC, Context.MODE_PRIVATE);
+                Set<String> storedAppIds = sharedPref.getStringSet(PreferenceKeys.SAVED_APP_IDS,
+                        new HashSet<String>());
+
+                // only update, if wifi or mobile data is on
+                if (activeInfo.getType() == ConnectivityManager.TYPE_MOBILE ||
+                        activeInfo.getType() == ConnectivityManager.TYPE_WIFI)
+                {
+                    UpdateWidgetTask.Mode updateMode = UpdateWidgetTask.Mode.ULTRA_SILENT;
+
+                    /*
+                    Only update in silent mode if switched to mobile data. Otherwise you are probably
+                    in wifi mode, ergo there is no new information about the used data and therefore
+                    grayish the progress bar.
+                     */
+                    if (activeInfo.getType() == ConnectivityManager.TYPE_MOBILE)
+                    {
+                        updateMode = UpdateWidgetTask.Mode.SILENT;
+                    }
+
+                    // make new task for every widget
+                    for (String storedAppId : storedAppIds)
+                    {
+                        int appWidgetId = Integer.valueOf(storedAppId.substring(0, storedAppId.
+                                indexOf(",")));
+                        String carrier = storedAppId.substring(storedAppId.indexOf(",") + 1);
+
+                        new UpdateWidgetTask(appWidgetId, context, updateMode, carrier)
+                                .executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+                    }
+                }
             }
         }
     }
